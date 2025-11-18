959ac97a3e6c97c38fd9f604aab0644f946c4113
Raghunandan Pratoori <rnp@iastate.edu>
2025-11-18 14:18:52 -0600

    check timesteps in a file

---
+++ problems/ts_num.py
@@ -0,0 +1,11 @@
+from netCDF4 import Dataset
+import glob
+
+for f in sorted(glob.glob("*.e*")):
+    try:
+        ds = Dataset(f)
+        times = ds.variables['time_whole'][:]
+        print(f"{f}: {len(times)} timesteps, time range = [{times[0]:.4e}, {times[-1]:.4e}]")
+        ds.close()
+    except Exception as e:
+        print(f"Skipping {f} ({e})")
